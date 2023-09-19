f11516ab69e3f2e3942ee868d63a31fe29315a38
RitikKumar256 <119650313+RitikKumar256@users.noreply.github.com>
2023-09-19 12:24:29 +0000

    conversion

---
+++ .devcontainer/01basic/03_conversionOperator.js
@@ -0,0 +1,20 @@
+let score="33abc"
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber=Number(score);
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber);
+
+//input         //output
+//"33"=>        33
+//"33abc"  =>   nan(not a number) type is number
+//true  => 1; false=>0;
+
+let isloggedin="";
+let booleanconvert=Boolean(isloggedin);
+console.log(booleanconvert);
+
+//1=true  0=false
+//""=> false
+//"ritik"=> true
