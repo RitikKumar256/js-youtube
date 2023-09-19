cfd5f1f15c37f728799300a142290af78d081d1d
RitikKumar256 <119650313+RitikKumar256@users.noreply.github.com>
2023-09-19 09:57:25 +0000

    datatypes

---
+++ .devcontainer/01basic/01_variable.js
@@ -0,0 +1,18 @@
+const accountId=14453
+let accountEmail="ritik@gogle.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+
+
+accountEmail="monu@gogle.com"
+accountPassword="45345"
+accountCity="khadana"
+let accountState;
+//accountId=2// not allowed
+console.log(accountId);
+
+/*
+prefer not use var
+because of issue in block scope and funtional scope
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
---
+++ .devcontainer/01basic/02_datatype.js
@@ -0,0 +1,22 @@
+"use strict";//treat all js code as newer version
+//alert(3+3)//we using nodejs not browser
+console.log(3
+    +3) //code readibilty should be high
+
+    console.log("ritik")
+
+    let name="ritik"
+    let age=22
+    let isloggedin=false
+    //number=2 to power 53
+    //bigint
+    //string=>""
+    //boolean=>true/false
+    //null=>standalone value
+    //undefined
+    //symbol=> unique
+
+    //object
+
+    console.log(typeof undefined);//undefined
+    console.log(typeof null);//object
