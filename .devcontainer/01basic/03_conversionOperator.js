13c57709cd8b294c2e433bdc4fc88e80cd2c1f7e
RitikKumar256 <119650313+RitikKumar256@users.noreply.github.com>
2023-09-20 09:39:54 +0000

    conversion

--- .devcontainer/01basic/03_conversionOperator.js
+++ .devcontainer/01basic/03_conversionOperator.js
@@ -13,8 +13,22 @@ let valueInNumber=Number(score);
 
 let isloggedin="";
 let booleanconvert=Boolean(isloggedin);
-console.log(booleanconvert);
+// console.log(booleanconvert);
 
 //1=true  0=false
 //""=> false
 //"ritik"=> true
+//*-----------operation-------*//
+//console.log(2**3);//gives power like 2 ki power 3
+// console.log("1"+2);//=>12
+// console.log(1+"2");//=>12
+// console.log("1"+2+3);//=>123
+// console.log(1+2+"3");//=>33
+// console.log(3+4*5%3);//o/p=>5
+console.log(true);//true
+console.log(+true);//1
+console.log(+"");//0
+
+let count=100;
+count++;
+console.log(count);//101
